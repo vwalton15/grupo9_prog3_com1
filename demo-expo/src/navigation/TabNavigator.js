6d640f5c9a28220516ff0e89f863e4e05cc05d80
Onabeitia <onabeitia@icloud.com>
2025-11-04 14:53:43 -0300

    haciendo perfil, eliminar post por resolver

--- demo-expo/src/navigation/TabNavigator.js
+++ demo-expo/src/navigation/TabNavigator.js
@@ -1,11 +1,11 @@
-import Home from '../screens/Home';
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import NewPost from '../screens/NewPost';
 import StackNavigator2 from './StackNavigator2';
+import Perfil from '../screens/Perfil';
 
 const Tab = createBottomTabNavigator();
 
@@ -14,6 +14,7 @@ export default function TabNavigator() {
         <Tab.Navigator screenOptions={{ tabBarShowLabel: false }}>
              <Tab.Screen name= "StackNavigator2" component={ StackNavigator2 } options={{ tabBarIcon: () => <FontAwesome name="home" size={24} color="black" />, headerShown: false}} />
              <Tab.Screen name= "NewPost" component={ NewPost } options={{ tabBarIcon: () => <Ionicons name="add-circle-outline" size={24} color="black" />, headerShown: false}} />
+             <Tab.Screen name= "Perfil" component={ Perfil } options={{ tabBarIcon: () => <MaterialCommunityIcons name="account-circle-outline" size={24} color="black" />, headerShown: false}} />
         </Tab.Navigator>
     )
 }
---
+++ demo-expo/src/screens/Perfil.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { View, Text, FlatList, Pressable } from 'react-native'
+import { db, auth } from '../firebase/config';
+import { Component } from 'react';
+import firebase from "firebase";
+
+
+export default class Comentarios extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            usuario: [],
+            posteos: [],
+        }
+    }
+    componentDidMount() {
+
+        db.collection("users").where('email', '==', auth.currentUser.email).onSnapshot((docs) => {
+            let usuarioDoc = []
+            docs.forEach((doc) => {
+                usuarioDoc.push({
+                    id: doc.id,
+                    email: doc.data().email,
+                    user: doc.data().user,
+                });
+            });
+            this.setState({ usuario: usuarioDoc });
+            console.log(this.state.usuario);
+
+            db.collection("posts").where('owner', '==', auth.currentUser.email).onSnapshot((docs) => {
+                let posteosDoc = []
+                docs.forEach((doc) => {
+                    posteosDoc.push({
+                        id: doc.id,
+                        owner: doc.data().owner,
+                        createdAt: doc.data().createdAt,
+                        post: doc.data().post,
+                    });
+                });
+                this.setState({ posteos: posteosDoc });
+                console.log(this.state.posteos);
+
+            });
+        });
+    }
+    eliminarPost(post) {
+            db.collection("users")
+                .doc(auth.currentUser.email)
+                .update({
+                    posteos: firebase.firestore.FieldValue.arrayRemove(post),
+                })
+                .then(() => console.log("Posteo eliminado"))
+                .catch((error) => console.log(error));
+                
+        }
+    render() {
+        return (
+            <View>
+                <Text>Usuario:</Text>
+                <FlatList
+                    data={this.state.usuario}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => <Text>{item.user}</Text>}
+                />
+                <Text>Mis Posteos:</Text>
+                {this.state.posteos.length != 0 ? (
+                    <FlatList
+                    data={this.state.posteos}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => ( <Text>{item.post}</Text> 
+                    )
+
+                }
+                />) : (<Text>No hay posteos aun</Text>)}
+                <Pressable onPress={() => auth.signOut()}>
+                    <Text>Desloguearse</Text>
+                </Pressable>
+                <Pressable onPress={() => this.eliminarPost(posteo)}>
+                      <Text>Eliminar Post</Text>
+                </Pressable>
+            </View>
+        )
+    }
+}
